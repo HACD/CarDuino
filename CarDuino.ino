7cdce7dca766cbb003494adbb4b4f658224413ad
Anthony Sellitti <anthony@hacd.com.au>
2012-07-21 22:07:39 +1000

    initial commit

---
+++ CarDuino.ino
@@ -0,0 +1,22 @@
+void setup()  
+{ 
+  // declare pin 9 to be an output:
+  pinMode(led, OUTPUT);
+} 
+
+void loop()  
+{ 
+  // set the brightness of pin 9:
+  analogWrite(led, brightness);    
+
+  // change the brightness for next time through the loop:
+  brightness = brightness + fadeAmount;
+
+  // reverse the direction of the fading at the ends of the fade: 
+  if (brightness == 0 || brightness == 255) 
+  {
+    fadeAmount = -fadeAmount ; 
+  }     
+  // wait for 30 milliseconds to see the dimming effect    
+  delay(30);                            
+}
