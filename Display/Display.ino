9eb135f27b34b654f78c8c059a5db3f9189a0301
Michael Chandler <michael@chandler.id.au>
2012-07-21 23:17:33 +1000

    Adding basic random generated display.

---
+++ Display/Display.ino
@@ -0,0 +1,66 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd( 8, 9, 4, 5, 6, 7 );
+
+#define COLUMNS = 2;
+#define ROWS = 16; 
+
+char randomCharacters[16] = {'#', '(', ')', '*', '+', '-', ':', ';', '<', '=', '>', '{', '}', '[', ']', '~'};
+
+char screen[16][2] = 
+{
+      {'0', '1'},
+      {'2', '3'},
+      {'4', '5'},
+      {'6', '7'},
+      {'8', '9'},
+      {'!', '@'},
+      {'#', '$'},
+      {'%', '^'},
+      {'&', '*'},
+      {'(', ')'},
+      {'_', '+'},
+      {',', '.'},
+      {'/', ';'},
+      {'<', '>'},
+      {'?', '|'},     
+      {'!', '!'}
+};  
+
+void setup()
+{  
+       lcd.begin(16, 2);
+}
+
+void loop()
+{
+   fillScreenRandomly();
+   display(screen);
+   delay(500);
+}
+
+void fillScreenRandomly() {
+  for (int row = 0; row < 16; row++) {    
+     for (int column = 0; column < 2; column++) {
+        char randomValue = randomCharacters[random(0, 16+1 /* exclusive */)];
+        screen[row][column] = randomValue;
+     }
+  }   
+}
+
+void display(char screen[16][2]) 
+{  
+  for (int row = 0; row < 16; row++) {    
+     for (int column = 0; column < 2; column++) {
+       int uprightColumn = toUprightColumn(column);
+       
+       lcd.setCursor(row, uprightColumn);       
+       lcd.print(screen[row][column]); 
+     }
+  } 
+}
+
+// when the screen is rotated 90 degrees clockwise, what was (0,0) becomes (1,0)
+int toUprightColumn(int horizontalColumn) {
+   return horizontalColumn == 0 ? 1 : 0; 
+}
