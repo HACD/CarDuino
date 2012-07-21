312d4301db7db7a2a698e3ea95006d17fb035072
Anthony Sellitti <anthony@hacd.com.au>
2012-07-22 01:09:12 +1000

    added basic player framework

---
+++ Player.ino
@@ -0,0 +1,25 @@
+/*--------------------------------------------------------------------------------------
+  Defines
+--------------------------------------------------------------------------------------*/
+
+// button voltage pin
+#define PLAYER_POSITON_LEFT           0
+#define PLAYER_POSITON_RIGHT          1
+#define PLAYER_AVATAR               'o'
+
+
+/*--------------------------------------------------------------------------------------
+  Variables
+--------------------------------------------------------------------------------------*/
+
+unsigned int player_position = PLAYER_POSITION_LEFT;
+
+unsigned int player_getPosition()
+{
+  return player_position;
+}
+
+void player_display()
+{
+  ;
+}
