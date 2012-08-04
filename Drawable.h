5c0440ebe610b60fde96e076de5ecce32d75c9a1
Anthony Sellitti <anthony@hacd.com.au>
2012-08-04 22:21:21 +1000

    added instance variables to Drawable

--- Drawable.h
+++ Drawable.h
@@ -3,10 +3,22 @@
 
 #include "Display.h"
 
+typedef struct co_ordinates_t 
+{
+  int x;
+  int y;
+} Coordinates;
+
 class Drawable
 {
+  private:
+    Display _display;
+    Coordinates _coordinates;
+    char** _avatar;
+    bool _dirty;
+    
   public:
-		Drawable(Display display);
+    Drawable(Display display);
 
     virtual void paint() = 0;
 };
